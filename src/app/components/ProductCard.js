b0392f68371a1a8decb52d0e177be16671ce9d9d
abhijain2004 <21mc3017@rgipt.ac.in>
2024-10-07 02:34:11 +0530

    final

--- src/app/components/ProductCard.js
+++ src/app/components/ProductCard.js
@@ -4,7 +4,9 @@ function ProductCard({image,title,id}) {
   return <div id={id} className={styles['card']}>
   <img src={image} className={styles['card-image']} alt="card-image" />
   <span className={styles['card-title']}>{title}</span>
-  <div style={{display:"flex",justifyContent:"space-between"}}><u>Sign in</u> or Create an account to see pricing
+  <div className={styles['card-footer']}>
+  <div><u>Sign in</u> or Create an account to see pricing
+  </div>
   <img src="assets/heart.png" height={30} width={30} alt="heart" style={{cursor:"pointer"}}/></div>
   </div>
 }
